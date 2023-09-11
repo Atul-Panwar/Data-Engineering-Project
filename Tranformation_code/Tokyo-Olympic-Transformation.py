978134d417f0e7b17604a590fb2163bdd0ff284a
Atul-Panwar <144582655+Atul-Panwar@users.noreply.github.com>
2023-09-11 10:25:06 +0530

    Add files via upload

---
+++ Tranformation_code/Tokyo-Olympic-Transformation.py
@@ -0,0 +1,114 @@
+# Databricks notebook source
+from pyspark.sql.functions import col
+from pyspark.sql.types import IntegerType, DoubleType, BooleanType, DateType
+
+# COMMAND ----------
+
+
+configs = {
+"fs.azure.account.auth.type": "OAuth",
+"fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+"fs.azure.account.oauth2.client.id": "da4dd214-6f29-454b-be69-e0c5b2b6e343",
+"fs.azure.account.oauth2.client.secret": 'Vyh8Q~PpL1IQ5HclJm~HIkdHnx.IasJSWhy1Zb7v',
+"fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/b2afac5a-6768-467c-99d1-e528c093ee06/oauth2/token"
+}
+
+dbutils.fs.mount(
+source = "abfss://tokyoolympics@tokyoolympicsstrgacc.dfs.core.windows.net", # contrainer@storageacc
+mount_point = "/mnt/tokyoolympic",
+extra_configs = configs)
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls "/mnt/tokyoolympic"
+
+# COMMAND ----------
+
+# Reading the raw data from the source location i.e raw-data folder in ADLS for doing transformation on the raw data
+athletes = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/mnt/tokyoolympic/raw-data/athletes.csv")
+coaches = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/mnt/tokyoolympic/raw-data/coaches.csv")
+entriesgender = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/mnt/tokyoolympic/raw-data/entriesgender.csv")
+medals = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/mnt/tokyoolympic/raw-data/medal.csv")
+teams = spark.read.format("csv").option("header","true").option("inferSchema","true").load("/mnt/tokyoolympic/raw-data/teams.csv")
+
+# COMMAND ----------
+
+athletes.show()
+
+# COMMAND ----------
+
+athletes.printSchema()
+
+# COMMAND ----------
+
+coaches.show()
+
+# COMMAND ----------
+
+coaches.printSchema()
+
+# COMMAND ----------
+
+entriesgender.show()
+
+# COMMAND ----------
+
+entriesgender.printSchema()
+
+# COMMAND ----------
+
+# Doing a transformation on entriesgender dataframe by changing the datastype to integer
+entriesgender = entriesgender.withColumn("Female",col("Female").cast(IntegerType()))\
+    .withColumn("Male",col("Male").cast(IntegerType()))\
+    .withColumn("Total",col("Total").cast(IntegerType()))
+
+# COMMAND ----------
+
+entriesgender.printSchema()
+
+# COMMAND ----------
+
+medals.show()
+
+# COMMAND ----------
+
+medals.printSchema()
+
+# COMMAND ----------
+
+teams.show()
+
+# COMMAND ----------
+
+teams.printSchema()
+
+# COMMAND ----------
+
+# Find the top countries with the highest number of gold medals
+top_gold_medal_countries = medals.orderBy("Gold", ascending=False).select("Team_Country","Gold").show()
+
+# COMMAND ----------
+
+# Calculate the average number of entries by gender for each discipline
+average_entries_by_gender = entriesgender.withColumn(
+    'Avg_Female', entriesgender['Female'] / entriesgender['Total']
+).withColumn(
+    'Avg_Male', entriesgender['Male'] / entriesgender['Total']
+)
+average_entries_by_gender.show()
+
+# COMMAND ----------
+
+# Writing or loading the transformed data into the target destination i.e transformed-data folder in ADLS
+
+athletes.repartition(1).write.mode("overwrite").option("header",'true').csv("/mnt/tokyoolymic/transformed-data/athletes")
+coaches.repartition(1).write.mode("overwrite").option("header","true").csv("/mnt/tokyoolymic/transformed-data/coaches")
+entriesgender.repartition(1).write.mode("overwrite").option("header","true").csv("/mnt/tokyoolymic/transformed-data/entriesgender")
+medals.repartition(1).write.mode("overwrite").option("header","true").csv("/mnt/tokyoolymic/transformed-data/medals")
+teams.repartition(1).write.mode("overwrite").option("header","true").csv("/mnt/tokyoolymic/transformed-data/teams")
+
+
+# COMMAND ----------
+
+
